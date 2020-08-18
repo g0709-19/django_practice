2701ce30b4715573aca57174a713d8b02f1eb21a
Joon Lee <sch030518@likelion.org>
2020-08-18 18:17:20 +0900

    검색 기능 구현

--- api_test/map.js
+++ api_test/map.js
@@ -11,6 +11,26 @@ var celsius = kelvin => {
     return kelvin - 273.15;
 }
 
+var getParameters = function (paramName) {
+    // 리턴값을 위한 변수 선언
+    var returnValue;
+
+    // 현재 URL 가져오기
+    var url = location.href;
+
+    // get 파라미터 값을 가져올 수 있는 ? 를 기점으로 slice 한 후 split 으로 나눔
+    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&');
+
+    // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
+    for (var i = 0; i < parameters.length; i++) {
+        var varName = parameters[i].split('=')[0];
+        if (varName.toUpperCase() == paramName.toUpperCase()) {
+            returnValue = parameters[i].split('=')[1];
+            return decodeURIComponent(returnValue);
+        }
+    }
+};
+
 // 좌표로부터 날씨 정보 가져옴
 function getWeatherFromCoord(lat, lon) {
     var result = "";
@@ -93,4 +113,5 @@ var createMap = function (id, lat, lon, _level) {
 
 // 지도 생성
 map = createMap('map', lat, lon, 3);
-searchPlace('순천향대학교');
+keyword = getParameters('place');
+searchPlace(keyword);
